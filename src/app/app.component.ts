2c6c34e2d2c1dc65f8b974d7d8f305e567fbf71e
Namrata <namratakudache09@gmail.com>
2024-06-17 17:24:10 +0530

    twowaybinding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,15 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  template: `<h1>Two Way Data Binding</h1>
+
+    <input [(ngModel)]="parentData" placeholder="Enter some text" />
+    <p>Parent Data: {{ parentData }}</p>
+
+    <app-test [(childData)]="parentData"></app-test>`,
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'TwowayBinding';
+  parentData: string = 'Initial parent data';
 }
